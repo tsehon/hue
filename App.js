ecc8485b654b2c3f6a2d709274d80f3680efd797
Tyler Sehon <tsehon@usc.edu>
2022-11-11 15:42:43 -0800

    searchbar

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, View, ScrollView } from 'react-native';
+
+const Searchbar = () => {
+  return (
+      <TextInput
+        style={styles.searchbar}
+        placeholder="Search"
+      />
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.page}>
+      <Searchbar/>
+      <View style={styles.container}>
+        <Text>hue</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  page: {
+    paddingTop: 60,
+    flex: 1,
+    width: '100%',
+  },
+  searchbar: {
+    height: 40,
+    width: '90%',
+    alignSelf: 'center',
+    textAlign: 'center',
+    borderColor: 'purple',
+    borderWidth: 1,
+  },
   container: {
     flex: 1,
+    paddingTop: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
