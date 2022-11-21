082b7293aa1c061d13226892b5ed0f14c3032284
Michael/Misha Khmelkov <khmelkov@usc.edu>
2022-11-20 22:01:18 -0800

    Video thumbnail generation

---
+++ components/Videos.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Button, View, Image, Text } from 'react-native';
+import * as VideoThumbnails from 'expo-video-thumbnails';
+
+
+// Pass in video uri as a prop and this will return a thumbnail of the video
+// Pass in style as prop
+const Thumbnail = (props) => {
+    const [image, setImage] = useState(null);
+    const [viduri, setVidUri] = useState(null);
+
+    useEffect(() => {
+        setVidUri(props.uri);
+        if (image == null && viduri != null) generateThumbnail();
+    });
+
+    const generateThumbnail = async () => {
+    try {
+        const { uri } = await VideoThumbnails.getThumbnailAsync(
+        viduri, { time: 15000, }
+        );
+        setImage(uri);
+    } catch (e) {
+        console.warn(e);
+    }
+    };
+
+    return (
+            <Image source={{ uri: image }} style={props.style} />
+      );
+}
+
+export default Thumbnail;
