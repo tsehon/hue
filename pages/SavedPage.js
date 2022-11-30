5d5b563fc9a02258baa23a32f15b72793fc476d1
Tyler Sehon <tsehon@usc.edu>
2022-11-29 19:06:11 -0800

    saved coming soon

--- pages/SavedPage.js
+++ pages/SavedPage.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FocusAwareStatusBar from '../components/FocusAwareStatusBar';
 
 export default function SavedPage({ navigation, route }) {
     return (
-        <SafeAreaView style={styles.page}>
+        <SafeAreaView style={styles.container}>
             <FocusAwareStatusBar barStyle='dark-content'/>
+            <Text style={styles.title}>Coming Soon</Text>
         </SafeAreaView >
     );
 }
 
 const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-        backgroundColor: 'white',
+    title: {
+        fontSize: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        fontFamily: 'PlusJakartaSans-SemiBold',
+        alignSelf: 'center'
     },
+    container: {
+        padding: 100,
+    }
 });
